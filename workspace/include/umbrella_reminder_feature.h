c7e847e32be21439ee9560d043d0f6ea1b78f32c
junseo.lee <ljs3696515@gmail.com>
2026-03-21 16:35:34 +0900

    add unmrella reminder features

---
+++ workspace/include/umbrella_reminder_feature.h
@@ -0,0 +1,8 @@
+/* define umbrella features */
+#ifndef __UMBRELLA_REMINDER_H__
+#define __UMBRELLA_REMINDER_H__
+#define FEATURE_UMBRELLA_ENABLE
+
+#ifdef FEATURE_UMBRELLA_ENABLE
+#endif /* FEATURE_UMBRELLA_ENABLE*/
+#endif /* __UMBRELLA_REMINDER_H__ */
