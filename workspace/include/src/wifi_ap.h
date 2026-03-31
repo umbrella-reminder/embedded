a501c198d3f911caf3f118b78b818e52f949d9f9
junseo.lee <ljs3696515@gmail.com>
2026-03-31 16:48:37 +0900

    add dns server task operation.

--- workspace/include/src/wifi_ap.h
+++ workspace/include/src/wifi_ap.h
@@ -2,7 +2,69 @@
 #define __WIFI_AP_H__
 #include "board.h"
 
+#define DNS_PACKET_SIZE_MAX           512
 #define CAPTIVE_PORTAL_URL_SIZE       32
+
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_LEN 512
+
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_FLAG_RD (1 << 0)
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_FLAG_TC (1 << 1)
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_FLAG_AA (1 << 2)
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_FLAG_QR (1 << 7)
+
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_A 1
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_NS 2
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_CNAME 5
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_SOA 6
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_WKS 11
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_PTR 12
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_HINFO 13
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_MINFO 14
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_MX 15
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_TXT 16
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_URI 256
+
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QCLASS_IN 1
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QCLASS_ANY 255
+#define WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QCLASS_URI 256
+
+typedef struct __attribute__((packed))
+{
+  uint16_t id;
+  uint8_t flags;
+  uint8_t rcode;
+  uint16_t qdcount;
+  uint16_t ancount;
+  uint16_t nscount;
+  uint16_t arcount;
+} dns_header;
+
+typedef struct __attribute__((packed))
+{
+  uint8_t len;
+  uint8_t data;
+} dns_label;
+
+typedef struct __attribute__((packed))
+{
+  uint16_t type;
+  uint16_t cl;
+} dns_question_footer;
+
+typedef struct __attribute__((packed))
+{
+  uint16_t type;
+  uint16_t cl;
+  uint32_t ttl;
+  uint16_t rdlength;
+} dns_resource_footer;
+
+typedef struct __attribute__((packed))
+{
+  uint16_t prio;
+  uint16_t weight;
+} dns_uri_hdr;
+
 
 int wifi_ap_init (void);
 #endif /* __WIFI_AP_H__*/
--- workspace/src/wifi_ap.c
+++ workspace/src/wifi_ap.c
@@ -146,7 +146,7 @@ esp_err_t
 http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
 {
 #if 1
-   httpd_resp_set_status(req, "302 Found");
+    httpd_resp_set_status(req, "302 Found");
     httpd_resp_set_hdr(req, "Location", "http://192.168.4.1");
     httpd_resp_send(req, NULL, 0);
 #else
@@ -184,78 +184,417 @@ start_web_server (void)
     return 0;
 }
 
+int
+dns_recive_hdr_check (dns_header *hdr, int size)
+{
+    if (NULL == hdr)
+    {
+        return 0;
+    }
+
+    if (size > DNS_PACKET_SIZE_MAX)
+    {
+        return 0;
+    }
+
+    if (size < sizeof (dns_header))
+    {
+        return 0;
+    }
+
+    if (hdr->ancount || hdr->nscount || hdr->arcount)
+    {
+        return 0;
+    }
+
+    if (hdr->flags & WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_FLAG_TC)
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
 #if 1
-// 수정된 DNS 서버 태스크 (패킷 ID 일치화)
-static void dns_server_task(void *pvParameters) {
-    uint8_t data[512];
-    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
-    struct sockaddr_in servaddr = {
-        .sin_family = AF_INET,
-        .sin_addr.s_addr = htonl(INADDR_ANY),
-        .sin_port = htons(53)
+static char *label_to_str(char *packet, char *labelPtr, int packetSz, char *res, int resMaxLen)
+{
+    int i = 0, j, k;
+    char *endPtr = NULL;
+
+    while (labelPtr >= packet && (labelPtr - packet) < packetSz && *labelPtr != 0)
+    {
+        if ((*labelPtr & 0xC0) == 0)
+        {
+            j = *labelPtr++; 
+            if (i < resMaxLen - 1 && i != 0)
+            {
+                res[i++] = '.';
+            }
+            
+            for (k = 0; k < j; k++)
+            {
+                if ((labelPtr - packet) >= packetSz)
+                {
+                    return NULL;
+                }
+                if (i < resMaxLen - 1)
+                {
+                    res[i++] = *labelPtr++;
+                }
+                else
+                {
+                    labelPtr++;
+                }
+            }
+        }
+        else if ((*labelPtr & 0xC0) == 0xC0)
+        {
+            if (endPtr == NULL)
+            {
+                endPtr = labelPtr + 2;
+            }
+            int offset = ntohs(*(uint16_t *)labelPtr) & 0x3FFF;
+            if (offset >= packetSz)
+            {
+                return NULL;
+            }
+            labelPtr = &packet[offset];
+        }
+        else
+        {
+          return NULL;
+        }
+    }
+
+    res[i] = 0; 
+    return (endPtr == NULL) ? labelPtr + 1 : endPtr;
+}
+
+static char *str_to_label(char *str, char *label, int maxLen)
+{
+    char *len = label;   
+    char *p = label + 1; 
+    
+    while (*str != 0)
+    {
+        if ((p - label) >= maxLen)
+        {
+            return NULL;
+        }
+
+        if (*str == '.')
+        {
+            *len = (uint8_t)((p - len) - 1);
+            len = p;                
+            p++;                    
+            str++;
+        }
+        else
+        {
+            *p++ = *str++;
+        }
+    }
+    
+    *len = (uint8_t)((p - len) - 1);
+    if ((p - label) >= maxLen)
+    {
+        return NULL; 
+    }
+    *p++ = 0; 
+    
+    return p;
+}
+#else
+static char *label_to_str(char *packet, char *labelPtr, int packetSz, char *res, int resMaxLen)
+{
+  int i, j, k;
+  char *endPtr = NULL;
+  i = 0;
+  do
+  {
+    if ((*labelPtr & 0xC0) == 0)
+    {
+      j = *labelPtr++; // skip past length
+      // Add separator period if there already is data in res
+      if (i < resMaxLen && i != 0)
+        res[i++] = '.';
+      // Copy label to res
+      for (k = 0; k < j; k++)
+      {
+        if ((labelPtr - packet) > packetSz)
+          return NULL;
+        if (i < resMaxLen)
+          res[i++] = *labelPtr++;
+      }
+    }
+    else if ((*labelPtr & 0xC0) == 0xC0)
+    {
+      // Compressed label pointer
+      endPtr = labelPtr + 2;
+      int offset = ntohs(*((short unsigned int *)labelPtr)) & 0x3FFF;
+      // Check if offset points to somewhere outside of the packet
+      if (offset > packetSz)
+        return NULL;
+      labelPtr = &packet[offset];
+    }
+    // check for out-of-bound-ness
+    if ((labelPtr - packet) > packetSz)
+      return NULL;
+  } while (*labelPtr != 0);
+  res[i] = 0; // zero-terminate
+  if (endPtr == NULL)
+    endPtr = labelPtr + 1;
+  return endPtr;
+}
+
+// Converts a dotted hostname to the weird label form dns uses.
+static char *str_to_label(char *str, char *label, int maxLen)
+{
+  char *len = label;   // ptr to len byte
+  char *p = label + 1; // ptr to next label byte to be written
+  while (1)
+  {
+    if (*str == '.' || *str == 0)
+    {
+      *len = ((p - len) - 1); // write len of label bit
+      len = p;                // pos of len for next part
+      p++;                    // data ptr is one past len
+      if (*str == 0)
+        break; // done
+      str++;
+    }
+    else
+    {
+      *p++ = *str++; // copy byte
+                     //if ((p-label)>maxLen) return NULL;	// check out of bounds
+    }
+  }
+  *len = 0;
+  return p; // ptr to first free byte in resp
+}
+#endif
+
+int
+dns_response_build (struct sockaddr_in *remote, uint8_t *data, int size, uint8_t *resp)
+{
+    char domain[DNS_PACKET_SIZE_MAX];
+    char question_count, i;
+
+    dns_header *data_hdr = (dns_header *) data;
+    dns_header *reply_hdr = (dns_header *) resp;
+    char *data_p = (char *)data;
+    char *resp_p = (char *)resp;
+
+    /* check null ptr */
+    if (NULL == remote || NULL == data || NULL == resp)
+    {
+        return 0;
+    }
+
+    /* check common dns header */
+    if (! dns_recive_hdr_check (data_hdr, size))
+    {
+        return 0;
+    }
+
+    /* copy origin data */
+    memcpy (resp, data, size);
+
+    /* set dns packet body */
+    data_p += size;
+
+    /* get reuqest count */
+    question_count = ntohs(data_hdr->qdcount);
+
+    reply_hdr->flags |= WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_FLAG_QR;
+
+    for (i = 0; i < question_count; i++)
+    {
+        /* get label in the body */
+        dns_question_footer *dns_footer = 
+          (dns_question_footer *)label_to_str((char *)data, data_p, 
+                                               size, domain, sizeof(domain));
+        if (dns_footer == NULL)
+        {
+            return 0;
+        }
+
+        /* set next data */
+        data_p = ((char *)dns_footer) + sizeof (dns_question_footer);
+
+        ESP_LOGI ("DNS-INFO","CAPTIVE Portal DNS qurey %s", domain);
+
+        if (ntohs (dns_footer->type) == WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_A)
+        {
+            esp_netif_ip_info_t ip_info;
+
+            dns_resource_footer *dns_res_footer = 
+              (dns_resource_footer*) str_to_label(domain, resp_p + size, (DNS_PACKET_SIZE_MAX - size));
+
+            if (dns_res_footer == NULL)
+            {
+                return 0;
+            }
+
+            resp_p = ((char *)dns_res_footer) + sizeof(dns_resource_footer);
+
+            dns_res_footer->type = htons(WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_A);
+            dns_res_footer->cl = htons(WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QCLASS_IN);
+            dns_res_footer->ttl = htons(0);
+            dns_res_footer->rdlength = htons(4);
+
+            esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_AP_DEF"), &ip_info);
+
+            resp_p[0] = ip4_addr1(&ip_info.ip);
+            resp_p[1] = ip4_addr2(&ip_info.ip);
+            resp_p[2] = ip4_addr3(&ip_info.ip);
+            resp_p[3] = ip4_addr4(&ip_info.ip);
+            resp_p += 4;
+
+            reply_hdr->ancount = htons(ntohs(reply_hdr->ancount) + 1);
+        }
+#if 0
+            else if (my_ntohs(&qf->type) == WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_NS)
+    {
+      // Give ns server. Basically can be whatever we want because it'll get resolved to our IP later anyway.
+      rend = str_to_label(buff, rend, sizeof(reply) - (rend - reply)); // Add the label
+      DnsResourceFooter *rf = (DnsResourceFooter *)rend;
+      rend += sizeof(DnsResourceFooter);
+      setn16(&rf->type, WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_NS);
+      setn16(&rf->cl, WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QCLASS_IN);
+      setn16(&rf->ttl, 0);
+      setn16(&rf->rdlength, 4);
+      *rend++ = 2;
+      *rend++ = 'n';
+      *rend++ = 's';
+      *rend++ = 0;
+      setn16(&rhdr->ancount, my_ntohs(&rhdr->ancount) + 1);
+    }
+    else if (my_ntohs(&qf->type) == WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_URI)
+    {
+      // Give uri to us
+      rend = str_to_label(buff, rend, sizeof(reply) - (rend - reply)); // Add the label
+      DnsResourceFooter *rf = (DnsResourceFooter *)rend;
+      rend += sizeof(DnsResourceFooter);
+      DnsUriHdr *uh = (DnsUriHdr *)rend;
+      rend += sizeof(DnsUriHdr);
+      setn16(&rf->type, WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QTYPE_URI);
+      setn16(&rf->cl, WIFI_CAPTIVE_PORTAL_ESP_IDF_DNS_QCLASS_URI);
+      setn16(&rf->ttl, 0);
+      setn16(&rf->rdlength, 4 + 16);
+      setn16(&uh->prio, 10);
+      setn16(&uh->weight, 1);
+      memcpy(rend, "http://esp.nonet", 16);
+      rend += 16;
+      setn16(&rhdr->ancount, my_ntohs(&rhdr->ancount) + 1);
+    }
+#endif
+    }
+
+    return (int) (resp_p - (char *)resp);
+}
+
+static void
+dns_server_task(void *arg)
+{
+    unsigned int count;
+    int sock = -1, ret;
+
+    uint8_t *data = NULL;
+    uint8_t *resp = NULL;
+    socklen_t len;
+
+    struct sockaddr_in remote_addr;
+    struct sockaddr_in server_addr =
+    {
+            .sin_family = AF_INET,
+            .sin_addr.s_addr = INADDR_ANY,
+            .sin_port = htons(53),
+            .sin_len = sizeof(struct sockaddr_in),
     };
-    bind(sock, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    /* create socket */
+    count = 0;
+    do
+    {
+        sock = socket (AF_INET, SOCK_DGRAM, IPPROTO_IP);
+        count++;
+
+        if (count > 100)
+        {
+            ESP_LOGI ("DNS-ERR", "socket create fail");
+            goto _err;
+        }
+
+        vTaskDelay (1000 / portTICK_PERIOD_MS);
+     } while (sock == -1);
+
+    ESP_LOGI ("DNS-INFO", "create socket(0x%x) for dns server ", sock);
+
+    /* bind socket */
+    count = 0;
+    do
+    {
+        ret = bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+        count++;
+
+        if (count > 100)
+        {
+            ESP_LOGI ("DNS-ERR", "socket bind erorr");
+            goto _err;
+        }
+
+        vTaskDelay (1000 / portTICK_PERIOD_MS);
+    }
+    while (ret != 0);
+
+    ESP_LOGI ("DNS-INFO", "create socket(0x%x) bind ok ", sock);
+
+    
+    data = malloc (DNS_PACKET_SIZE_MAX);
+    if (NULL == data)
+    {
+        ESP_LOGI ("DNS-ERR", "Memory allocation fail");
+        goto _err;
+    }
+
+    resp = malloc (DNS_PACKET_SIZE_MAX);
+    if (NULL == data)
+    {
+        ESP_LOGI ("DNS-ERR", "Memory allocation fail");
+        goto _err;
+    }
+
+    len = sizeof (remote_addr);
 
     while (1) {
-        struct sockaddr_in client_addr;
-        socklen_t addr_len = sizeof(client_addr);
-        int length = recvfrom(sock, data, sizeof(data), 0, (struct sockaddr *)&client_addr, &addr_len);
-        
-        if (length > 12) {
-            // 수신 패킷의 ID를 그대로 사용 (매우 중요!)
-            data[2] |= 0x80; // QR = 1 (Response)
-            data[3] |= 0x80; // RA = 1 (Recursion Available)
-            data[7] = 1;     // Answer count = 1
+        int size;
+        int send_size;
+        memset (&remote_addr, 0, len);
 
-            // 응답 데이터 (모든 질의에 192.168.4.1 응답)
-            uint8_t answer[] = {
-                0xc0, 0x0c, 0x00, 0x01, 0x00, 0x01,
-                0x00, 0x00, 0x00, 0x3c, // TTL 60
-                0x00, 0x04, 192, 168, 4, 1
-            };
-            memcpy(data + length, answer, sizeof(answer));
-            sendto(sock, data, length + sizeof(answer), 0, (struct sockaddr *)&client_addr, addr_len);
+        size = recvfrom(sock, data, DNS_PACKET_SIZE_MAX,
+                        0, (struct sockaddr *)&remote_addr, (socklen_t *)&len);
+
+        send_size = dns_response_build (&remote_addr, data, size, resp);
+
+        if (size > 0 && send_size > 0)
+        {
+            /* send */
+            sendto (sock, resp, send_size, 0, (struct sockaddr *)&remote_addr, sizeof(struct sockaddr_in));
         }
         vTaskDelay(1);
     }
-}
-#else
 
-// DNS 서버 태스크: 모든 도메인 질의에 ESP32 IP(192.168.4.1)로 응답
-static void dns_server_task(void *pvParameters) {
-    uint8_t data[128];
-    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
-    struct sockaddr_in servaddr = {
-        .sin_family = AF_INET,
-        .sin_addr.s_addr = htonl(INADDR_ANY),
-        .sin_port = htons(53) // DNS 표준 포트
-    };
-    bind(sock, (struct sockaddr *)&servaddr, sizeof(servaddr));
-
-    while (1) {
-        struct sockaddr_in client_addr;
-        socklen_t addr_len = sizeof(client_addr);
-        int length = recvfrom(sock, data, sizeof(data), 0, (struct sockaddr *)&client_addr, &addr_len);
-        if (length > 12) { // DNS 헤더 최소 크기
-            data[2] |= 0x80; // Response flag
-            data[3] |= 0x80; // Recursion available
-            data[7] = 1;     // Answer count = 1
-            
-            // 응답 패킷 조립 (단순화된 버전: 모든 질의에 192.168.4.1 응답)
-            uint8_t answer[] = {
-                0xc0, 0x0c,             // Name offset
-                0x00, 0x01, 0x00, 0x01, // Type A, Class IN
-                0x00, 0x00, 0x00, 0x3c, // TTL 60s
-                0x00, 0x04,             // Data length
-                192, 168, 4, 1          // IP Address
-            };
-            memcpy(data + length, answer, sizeof(answer));
-            sendto(sock, data, length + sizeof(answer), 0, (struct sockaddr *)&client_addr, addr_len);
-        }
-        vTaskDelay(pdMS_TO_TICKS(10));
+_err:
+    if (data)
+    {
+      free (data);
     }
+
+    close(sock);
+    vTaskDelete(NULL);
 }
-#endif
+
 int
 start_dns_server(void)
 {
